57f083f898ed774da610739dccfeaa7e9b2bfa70
Nelson Cabrera <80597467+onairhn7@users.noreply.github.com>
2024-03-18 14:55:07 -0600

    creacion de registro

--- src/components/InicioSesion.jsx
+++ src/components/InicioSesion.jsx
@@ -1,29 +1,72 @@
 import React from 'react'
 
 
-export const InicioSesion = () =>{
-    return(
+export const InicioSesion = () => {
+    return (
         <>
-        <div className='container mt-5' >
-            <form onSubmit={submitHandler} >
-                <fieldset>
-                    <legend>Inicio Sesion</legend>
+            <div className='container mt-5'>
 
-                    <div className="form-group row">
-                        <label className="col-sm-2 col-form-label">Contraseña</label>
-                        <div className="col-sm-10">
-                            <input type="password" className="form-control-plaintext"
-                                name="pass"
-                                onChange={onChangeHandler} />
+            <form>
+                <fieldset>
+                    <legend>Registro de Usuarios</legend>
+
+                    <div class="row">
+                        <label for="staticNombreUsuario" class="col-sm-2 col-form-label">Nombre Usuario</label>
+                        <div class="col-sm-10">
+                            <input type="text" readonly="" class="form-control-plaintext" id="staticNombreUsuario" placeholder="Seleccione su Usuario" />
                         </div>
                     </div>
 
-                    <button type="submit" className="btn btn-primary w-100">Iniciar Sesion</button>
+                    <div class="row">
+                        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="text" readonly="" class="form-control-plaintext" id="staticEmail" placeholder="email@example.com" />
+                        </div>
+                    </div>
+
+                    
+
+                    <div class="row">
+                        <label for="staticNombre" class="col-sm-2 col-form-label">Nombre</label>
+                        <div class="col-sm-10">
+                            <input type="text" readonly="" class="form-control-plaintext" id="staticNombre" placeholder="Escribe tu nombre" />
+                        </div>
+                    </div>
+
+                    
+
+                    <div class="row">
+                        <label for="staticApellido" class="col-sm-2 col-form-label">Apellido</label>
+                        <div class="col-sm-10">
+                            <input type="text" readonly="" class="form-control-plaintext" id="staticApellido" placeholder="Ingrese su Apellido" />
+                        </div>
+                    </div>
+
+
+                    <div class="row">
+                        <label for="staticConstrasena" class="col-sm-2 col-form-label">Contrasena</label>
+                        <div class="col-sm-10">
+                            <input type="text" readonly="" class="form-control-plaintext" id="staticConstrasena" placeholder="Ingrese su Contrasena" />
+                        </div>
+                    </div>
+
+
+                    <div class="row">
+                        <label for="staticConfirmacionCon" class="col-sm-2 col-form-label">Contrasena</label>
+                        <div class="col-sm-10">
+                            <input type="text" readonly="" class="form-control-plaintext" id="staticConfirmacionCon" placeholder="Confirme su Contrasena" />
+                        </div>
+                    </div>
+
+
+                    
+
+                    <button type="submit" class="btn btn-primary w-100">Crear Usuario</button>
                 </fieldset>
             </form>
-            <div> {inicioSesion} </div>
-        </div>
-    </>
+            </div>
+
+        </>
     )
 }
 
