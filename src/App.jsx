8c96756069f64d0a1a08879f6263756e565eb7f7
Nelson Cabrera <80597467+onairhn7@users.noreply.github.com>
2024-03-18 10:38:41 -0600

    actualizacion

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { InicioSesion } from './components/InicioSesion.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <InicioSesion />
+    </div>
+  );
+};
+
+export default App;
+
